48d30da2ab7930c6c109d0ac3687001636d25c28
Ishita Laller <s4020999@student.rmit.edu.au>
2024-10-20 06:59:38 +1100

    test cases with 2 data sets

--- assessment4se/src/assessment4se/PrescriptionTest.java
+++ assessment4se/src/assessment4se/PrescriptionTest.java
@@ -8,55 +8,85 @@ public class PrescriptionTest {
     @Test
     public void testAddPrescriptionValid() {
         // Test case 1: Valid prescription
-        Date examDate = new Date();
-        Prescription prescription = new Prescription(1, "Jane", "Oliver", 
-            "1 Koonalda Ave, Glen Waverly, 1001", 10f, 20f, 2f, examDate, "Dr. William");
-        assertTrue(prescription.addPrescription());
+        Date examDate1 = new Date();
+        Prescription prescription1 = new Prescription(1, "Jane", "Oliver", 
+            "1 Koonalda Ave, Glen Waverly, 1001", 10f, 20f, 2f, examDate1, "Dr. William");
+        assertTrue(prescription1.addPrescription());
+        
+        Date examDate2 = new Date();
+        Prescription prescription2 = new Prescription(1, "Jane", "Oliver", 
+            "1 Koonalda Ave, Glen Waverly, 1001", 15f, 40f, 1f, examDate2, "Dr. Rosanne");
+        assertTrue(prescription2.addPrescription());
     }
     
     @Test
     public void testAddPrescriptionInValidfirstname() {
-        // Test case 2: Invalid prescription
-        Date examDate = new Date();
-        Prescription prescription = new Prescription(2, "J", "Oliver", 
-            "1 Koonalda Ave, Glen Waverly, 1001", 10f, 20f, 2f, examDate, "Dr. William");
-        assertTrue(prescription.addPrescription());
+        // Test case 2: Invalid prescription (FirstName)
+        Date examDate1 = new Date();
+        Prescription prescription1 = new Prescription(2, "J", "Oliver", 
+            "1 Koonalda Ave, Glen Waverly, 1001", 10f, 20f, 2f, examDate1, "Dr. William");
+        assertFalse(prescription1.addPrescription());
+        
+        Date examDate2 = new Date();
+        Prescription prescription2 = new Prescription(2, "A", "Oliver", 
+            "1 Koonalda Ave, Glen Waverly, 1001", 10f, 20f, 2f, examDate2, "Dr. William");
+        assertFalse(prescription2.addPrescription());
     }
     
     @Test
     public void testAddPrescriptionInValidAddress() {
-        // Test case 3: Invalid prescription
-        Date examDate = new Date();
-        Prescription prescription = new Prescription(3, "Jane", "Oliver", 
-            "1 Koonalda Ave", 10f, 20f, 2f, examDate, "Dr. William");
-        assertTrue(prescription.addPrescription());
+        // Test case 3: Invalid prescription (Address)
+        Date examDate1 = new Date();
+        Prescription prescription1 = new Prescription(3, "Jane", "Oliver", 
+            "1 Koonalda Ave", 10f, 20f, 2f, examDate1, "Dr. William");
+        assertFalse(prescription1.addPrescription());
+        
+        Date examDate2 = new Date();
+        Prescription prescription2 = new Prescription(3, "Jane", "Oliver", 
+            "Glen Waverly", 10f, 20f, 2f, examDate2, "Dr. William");
+        assertFalse(prescription2.addPrescription());
     }
     
     @Test
     public void testAddPrescriptionInValidsphere() {
-        // Test case 4: Invalid prescription
-        Date examDate = new Date();
-        Prescription prescription = new Prescription(4, "Jane", "Oliver", 
-            "1 Koonalda Ave, Glen Waverly, 1001", -30f, 20f, 2f, examDate, "Dr. William");
-        assertTrue(prescription.addPrescription());
+        // Test case 4: Invalid prescription (Sphere value)
+        Date examDate1 = new Date();
+        Prescription prescription1 = new Prescription(4, "Jane", "Oliver", 
+            "1 Koonalda Ave, Glen Waverly, 1001", -30f, 20f, 2f, examDate1, "Dr. William");
+        assertFalse(prescription1.addPrescription());
+        
+        Date examDate2 = new Date();
+        Prescription prescription2 = new Prescription(4, "Jane", "Oliver", 
+            "1 Koonalda Ave, Glen Waverly, 1001", 50f, 20f, 2f, examDate2, "Dr. William");
+        assertFalse(prescription2.addPrescription());
     }
     
     @Test
     public void testAddPrescriptionInValidcylinder() {
-        // Test case 5: Invalid prescription
-        Date examDate = new Date();
-        Prescription prescription = new Prescription(5, "James", "Oliver", 
-            "1 Koonalda Ave, Glen Waverly, 1001", 10f, 20f, 5f, examDate, "Dr. William");
-        assertTrue(prescription.addPrescription());
+        // Test case 5: Invalid prescription (cylinder value)
+        Date examDate1 = new Date();
+        Prescription prescription1 = new Prescription(5, "James", "Oliver", 
+            "1 Koonalda Ave, Glen Waverly, 1001", 10f, 20f, 5f, examDate1, "Dr. William");
+        assertFalse(prescription1.addPrescription());
+        
+        Date examDate2 = new Date();
+        Prescription prescription2 = new Prescription(5, "James", "Oliver", 
+            "1 Koonalda Ave, Glen Waverly, 1001", 10f, 20f, -5f, examDate2, "Dr. William");
+        assertFalse(prescription2.addPrescription());
     }
     
     @Test
     public void testAddPrescriptionInValidaxis() {
-        // Test case 6: Invalid prescription
-        Date examDate = new Date();
-        Prescription prescription = new Prescription(6, "James", "Oliver", 
-            "1 Koonalda Ave, Glen Waverly, 1001", 10f, 200f, 3f, examDate, "Dr. William");
-        assertTrue(prescription.addPrescription());
+        // Test case 6: Invalid prescription (Axis value)
+        Date examDate1 = new Date();
+        Prescription prescription1 = new Prescription(6, "James", "Oliver", 
+            "1 Koonalda Ave, Glen Waverly, 1001", 10f, 200f, 3f, examDate1, "Dr. William");
+        assertFalse(prescription1.addPrescription());
+        
+        Date examDate2 = new Date();
+        Prescription prescription2 = new Prescription(6, "James", "Oliver", 
+            "1 Koonalda Ave, Glen Waverly, 1001", 10f, 200f, 3f, examDate2, "Dr. William");
+        assertFalse(prescription2.addPrescription());
     }
 
 }
--- assessment4se/src/assessment4se/remarksTest.java
+++ assessment4se/src/assessment4se/remarksTest.java
@@ -9,55 +9,85 @@ public class remarksTest {
     @Test
     public void testAddRemarkValid() {
         // Test case 1: Valid remark
-        Date examDate = new Date();
-        Prescription prescription = new Prescription(1, "Jane", "Oliver", 
-            "1 Koonalda Ave, Glen Waverly, 1001", 10f, 20f, 2f, examDate, "William");
-        assertTrue(prescription.addRemark("The service was excellent and professional.", "client"));
+        Date examDate1 = new Date();
+        Prescription prescription1 = new Prescription(1, "Jane", "Oliver", 
+            "1 Koonalda Ave, Glen Waverly, 1001", 10f, 20f, 2f, examDate1, "Dr. William");
+        assertTrue(prescription1.addRemark("The service was excellent and professional.", "client"));
+        
+        Date examDate2 = new Date();
+        Prescription prescription2 = new Prescription(1, "Jane", "Oliver", 
+            "1 Koonalda Ave, Glen Waverly, 1001", 10f, 20f, 2f, examDate2, "Dr. William");
+        assertTrue(prescription2.addRemark("This is the customer remark which should be added.", "client"));
     }
 
     @Test
     public void testAddRemarktooFewWords() {
-        // Test case 2: Invalid remark
-        Date examDate = new Date();
-        Prescription prescription = new Prescription(1, "Jane", "Oliver", 
-            "1 Koonalda Ave, Glen Waverly, 1001", 10f, 20f, 2f, examDate, "William");
-        assertTrue(prescription.addRemark("Nice", "client"));
+        // Test case 2: Invalid remark (few words)
+        Date examDate1 = new Date();
+        Prescription prescription1 = new Prescription(1, "Jane", "Oliver", 
+            "1 Koonalda Ave, Glen Waverly, 1001", 10f, 20f, 2f, examDate1, "Dr. William");
+        assertFalse(prescription1.addRemark("Nice", "client"));
+        
+        Date examDate2 = new Date();
+        Prescription prescription2 = new Prescription(1, "Jane", "Oliver", 
+            "1 Koonalda Ave, Glen Waverly, 1001", 10f, 20f, 2f, examDate2, "Dr. William");
+        assertFalse(prescription2.addRemark("Good", "client"));
     }
 
     @Test
     public void testAddRemarksTooManyWords() {
-        // Test case 3: Invalid remark
-        Date examDate = new Date();
-        Prescription prescription = new Prescription(1, "Jane", "Oliver", 
-            "1 Koonalda Ave, Glen Waverly, 1001", 10f, 20f, 2f, examDate, "William");
-        assertTrue(prescription.addRemark("The service was excellent and professional.The service was excellent and professional. The service was excellent and professional. The service was excellent and professional.", "client"));
+        // Test case 3: Invalid remark (many words)
+        Date examDate1 = new Date();
+        Prescription prescription1 = new Prescription(1, "Jane", "Oliver", 
+            "1 Koonalda Ave, Glen Waverly, 1001", 10f, 20f, 2f, examDate1, "Dr. William");
+        assertFalse(prescription1.addRemark("The service was excellent and professional.The service was excellent and professional. The service was excellent and professional. The service was excellent and professional.", "client"));
+ 
+        Date examDate2 = new Date();
+        Prescription prescription2 = new Prescription(1, "Jane", "Oliver", 
+            "1 Koonalda Ave, Glen Waverly, 1001", 10f, 20f, 2f, examDate2, "Dr. William");
+        assertFalse(prescription2.addRemark("This is the customer remark which should not be added.This is the customer remark which should not be added.This is the customer remark which should not be added.", "client"));
     }
 
     @Test
     public void testAddRemarkNoUppercase() {
-        // Test case 4: Invalid remark
-        Date examDate = new Date();
-        Prescription prescription = new Prescription(1, "Jane", "Oliver", 
-            "1 Koonalda Ave, Glen Waverly, 1001", 10f, 20f, 2f, examDate, "William");
-        assertTrue(prescription.addRemark("the service was excellent and professional.", "client"));
+        // Test case 4: Invalid remark (no upper case)
+        Date examDate1 = new Date();
+        Prescription prescription1 = new Prescription(1, "Jane", "Oliver", 
+            "1 Koonalda Ave, Glen Waverly, 1001", 10f, 20f, 2f, examDate1, "Dr. William");
+        assertFalse(prescription1.addRemark("the service was excellent and professional.", "client"));
+        
+        Date examDate2 = new Date();
+        Prescription prescription2 = new Prescription(1, "Jane", "Oliver", 
+            "1 Koonalda Ave, Glen Waverly, 1001", 10f, 20f, 2f, examDate2, "Dr. William");
+        assertFalse(prescription2.addRemark("this is the customer remark which should not be added.", "client"));
     }
 
     @Test
     public void testAddRemarkWrongRemark() {
-        // Test case 5: Invalid remark
-        Date examDate = new Date();
-        Prescription prescription = new Prescription(1, "Jane", "Oliver", 
-            "1 Koonalda Ave, Glen Waverly, 1001", 10f, 20f, 2f, examDate, "William");
-        assertTrue(prescription.addRemark("The service was excellent and professional.", "John"));
+        // Test case 5: Invalid remark (wrong remark type)
+        Date examDate1 = new Date();
+        Prescription prescription1 = new Prescription(1, "Jane", "Oliver", 
+            "1 Koonalda Ave, Glen Waverly, 1001", 10f, 20f, 2f, examDate1, "Dr. William");
+        assertFalse(prescription1.addRemark("The service was excellent and professional.", "John"));
+        
+        Date examDate2 = new Date();
+        Prescription prescription2 = new Prescription(1, "Jane", "Oliver", 
+            "1 Koonalda Ave, Glen Waverly, 1001", 10f, 20f, 2f, examDate2, "Dr. William");
+        assertFalse(prescription2.addRemark("The service was excellent and professional.", "other"));
     }
 
     @Test
     public void testAddRemarkSecondValid() {
-        // Test case 6: Valid remark
+        // Test case 6: Valid remark (valid data)
+        Date examDate1 = new Date();
+        Prescription prescription1 = new Prescription(1, "Jane", "Oliver", 
+            "1 Koonalda Ave, Glen Waverly, 1001", 10f, 20f, 2f, examDate1, "Dr. William");
+        assertTrue(prescription1.addRemark("The client liked the service and said is going to become a regular.", "optometrist"));
+        
         Date examDate = new Date();
         Prescription prescription = new Prescription(1, "Jane", "Oliver", 
-            "1 Koonalda Ave, Glen Waverly, 1001", 10f, 20f, 2f, examDate, "William");
-        assertTrue(prescription.addRemark("The client liked the service and said is going to become a regular.", "optometrist"));
+            "1 Koonalda Ave, Glen Waverly, 1001", 10f, 20f, 2f, examDate, "Dr. William");
+        assertTrue(prescription.addRemark("The customer said he liked how staff was so friendly and cooperative.", "optometrist"));
     }
 }
 
